f0f0caa888f037cbe2bcc4d4eda8ff2281f3f7e1
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-07-19 11:43:08 +0200

    start to display burger menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { Router } from '@angular/router';
 import { CreateColonyComponent } from './components/create-colony/create-colony.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
     RegisterComponent,
     CreateColonyComponent,
     LoginComponent,
+    HeaderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
