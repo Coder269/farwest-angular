86312313b70e35424870b1751c230181e43362c4
Mouhammad Zaher abbas <z.abbas83@yahoo.com>
2023-07-26 14:53:08 +0200

    Added colony service, and user model!

---
+++ src/app/Models/User.ts
@@ -0,0 +1,25 @@
+import { Colonie } from '../interfaces/colonie';
+
+export class User {
+  id?: number;
+  colonies?: Colonie[];
+  username: string;
+  password: string;
+  avatar: string;
+  money: number;
+  level: number;
+
+  constructor(
+    username: string,
+    password: string,
+    avatar: string,
+    money: number,
+    level: number
+  ) {
+    this.username = username;
+    this.password = password;
+    this.avatar = avatar;
+    this.money = money;
+    this.level = level;
+  }
+}
--- src/app/interfaces/colonie.ts
+++ src/app/interfaces/colonie.ts
@@ -1,4 +1,11 @@
+import { User } from '../Models/User';
+
 export interface Colonie {
-    colonyName: String;
-    colonyPicture: String;
+  colonyName: String;
+  colonyPicture: String;
+  user: User;
+  lastStrike: Date;
+  woodLastRecolt: Date;
+  goldLastRecolt: Date;
+  ironLastRecolt: Date;
 }
