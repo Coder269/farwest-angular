2b39081102895c86db86e4f00f9666e83477ebb7
[dervieux.ben@gmail.com](mailto:dervieux.ben@gmail.com) <dervieux.ben@gmail.com>
2023-07-31 16:32:40 +0200

    colonies can be display on map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Colonie } from 'src/app/interfaces/colonie';
+import { Services } from 'src/app/services/Services';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
+  randomColonies: Colonie[] = [];
 
+  constructor(private service: Services) {}
+
+  ngOnInit() {
+    this.randomColonies = this.service.getRandomColonies(9); //nombre de colonies à afficher
+  }
 }
