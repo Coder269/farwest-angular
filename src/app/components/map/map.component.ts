f6d7cdc1f67ced53480b9dd99f2f96599a92294d
Mouhammad Zaher abbas <z.abbas83@yahoo.com>
2023-08-01 15:20:22 +0200

    Added map logic!

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -9,14 +9,40 @@ import { ColonyService } from 'src/app/services/colony.service';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
-  colonies: Colonie[] = [];
+  allColonies: Colonie[] = [];
+  coloniesUser: Colonie[] = [];
+  otherColonies: Colonie[] = [];
 
   constructor(private colonyService: ColonyService) {}
 
   ngOnInit() {
     this.colonyService.getAllColonies().subscribe({
-      next: (response: Colonie[]) => (this.colonies = response),
+      next: (response: Colonie[]) => {
+        this.allColonies = response;
+        this.getColoniesOfUser();
+      },
       error: (error: HttpErrorResponse) => console.log(error.message),
     });
+  }
+
+  getColoniesOfUser() {
+    const userId = localStorage.getItem('userId');
+
+    if (userId)
+      this.colonyService.getColoniesOfUser(
+        parseInt(userId),
+        (response: Colonie[]) => {
+          this.coloniesUser = response;
+          this.getOtherColonies();
+        }
+      );
+  }
+
+  getOtherColonies() {
+    for (let colony of this.coloniesUser) {
+      this.otherColonies = this.allColonies.filter(
+        (colony2) => colony.id != colony2.id
+      );
+    }
   }
 }
